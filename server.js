3df0548e3e4b226e693be76220acb5e2f85445e0
GabrielG246 <garcia-gabriel-246@hotmail.com>
2022-12-05 17:37:06 -0300

    trial

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('cityList.json');
+const citiesRouter = jsonServer.router('citiesList.json');
+const countriesRouter= jsonServer.router('countriesList.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+server.use('/cities',citiesRouter);
+server.use('/countries',countriesRouter);
 
 server.listen(port);
